243a1dc312460a5f5a6b2bae7bd06dc309dfd5e9
Egor Kochetoff <gluk47@gmail.com>
2015-07-11 01:24:38 +0300

    Ну вот и глава 3 перевелась.
    Или пересказалась. Я был не слишком педантично близок к тексту :)
    Как всегда.

---
+++ ru/03.house.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2
+
+import turtle
+import sys
+t = turtle.Pen()
+t.forward (100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.right(135)
+t.forward(71)
+t.right(90)
+t.forward(71)
+t.right(90)
+t.forward(141)
+t.right(135)
+t.forward(100)
+t.right(135)
+t.forward(141)
+
+sys.stdin.readline()
