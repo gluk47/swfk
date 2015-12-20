0ae78c4297b0d3fd9ed8389a74a625cd04db3afa
Egor Kochetov <gluk47@gmail.com>
2015-12-20 17:35:49 +0300

    Собраны три версии книги для трёх операционок
    Надо ещё в главе 1 вернуть иллюстрации для разных ОС, и всё будет готово.

--- ru/build.sh
+++ ru/build.sh
@@ -1,2 +1,21 @@
-#!/bin/sh
-xelatex book.tex
+#!/bin/bash -e
+
+build () {
+	local os=$1
+	ln -fs version-${os}.tex version.tex
+	xelatex book.tex
+	mv book.pdf book-${os}.pdf
+}
+
+if [[ $1 ]]; then
+	for os in "$@"; do
+		build $os
+	done
+else
+	for i in {1..2}; do
+		for os in linux windows macos; do
+			build $os
+		done
+	done
+fi
+
